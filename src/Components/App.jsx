c52433636fbeff9dfca3ed2842eb302b7dfcfe19
Gagan Preet <gaganpreet.dev@gmail.com>
2021-05-20 21:47:38 +0530

    change button

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,84 +1,136 @@
 import React, { useState } from "react";
-import Clickables from "./Clickables";
+// import Clickables from "./Clickables";
+import Button from "@material-ui/core/Button";
 
 function App() {
-  const [str, updateStr] = useState("");
-  const [arr, updateArr] = useState([]);
-
-  function displayCalc(val) {
-    updateArr((prevValue) => {
-      return [...prevValue, val];
-    });
-
-    // console.log(arr);
+  // const [str, updateStr] = useState("");
+  // const [arr, updateArr] = useState([]);
+  function handleClick(e) {
+    console.log(e.currentTarget.value);
   }
-  function displayResult() {
-    updateStr(() => arr.join(""));
-    console.log("test:" + str);
-  }
+  // function displayCalc(val) {
+  //   updateArr((prevValue) => {
+  //     return [...prevValue, val];
+  //   });
+
+  //   console.log(arr);
+  // }
+  // function displayResult() {
+  //   updateStr(() => arr.join(""));
+  //   console.log("test:" + str);
+  // }
   return (
     <div className="container">
-      <h3>{str}</h3>
+      <h3>hello</h3>
       <h1>{}</h1>
       <div className="main">
         <div className="inner">
-          <Clickables val="C" handleClick={displayCalc} cName="whiteIcons" />
-          <Clickables val="7" handleClick={displayCalc} />
-          <Clickables val="4" handleClick={displayCalc} />
-          <Clickables val="1" handleClick={displayCalc} />
-          <Clickables val="0" handleClick={displayCalc} />
+          <Button value="C" id="whiteIcons" onClick={handleClick}>
+            C
+          </Button>
+          <Button value="7" onClick={handleClick}>
+            7
+          </Button>
+          <Button value="4" onClick={handleClick}>
+            4
+          </Button>
+          <Button value="1" onClick={handleClick}>
+            1
+          </Button>
+          <Button value="0" onClick={handleClick}>
+            0
+          </Button>
         </div>
         <div className="inner">
-          <Clickables
-            val={
+          <Button
+            value={
               <img
                 src="https://img.icons8.com/material/24/ffffff/square-root--v1.png"
                 alt="square root"
               />
             }
-            handleClick={displayCalc}
-            cName="icons"
-          />
+            id="icons"
+            onClick={handleClick}
+          >
+            <img
+              src="https://img.icons8.com/material/24/ffffff/square-root--v1.png"
+              alt="square root"
+            />
+          </Button>
 
-          <Clickables val="8" handleClick={displayCalc} />
-          <Clickables val="5" handleClick={displayCalc} />
-          <Clickables val="2" handleClick={displayCalc} />
-          <Clickables val="." handleClick={displayCalc} />
+          <Button value="8" onClick={handleClick}>
+            8
+          </Button>
+          <Button value="5" onClick={handleClick}>
+            5
+          </Button>
+          <Button value="2" onClick={handleClick}>
+            2
+          </Button>
+          <Button value="." onClick={handleClick}>
+            .
+          </Button>
         </div>
         <div className="inner">
-          <Clickables
-            val={
+          <Button
+            value={
               <img
                 src="https://img.icons8.com/material-outlined/24/ffffff/square-number.png"
                 alt="square of number"
               />
             }
-            handleClick={displayCalc}
-            cName="icons"
-          />
-          <Clickables val="9" handleClick={displayCalc} />
-          <Clickables val="6" handleClick={displayCalc} />
-          <Clickables val="3" handleClick={displayCalc} />
-          <Clickables
-            val={
+            onClick={handleClick}
+            id="icons"
+          >
+            <img
+              src="https://img.icons8.com/material-outlined/24/ffffff/square-number.png"
+              alt="square of number"
+            />
+          </Button>
+          <Button value="9" onClick={handleClick}>
+            9
+          </Button>
+          <Button value="6" onClick={handleClick}>
+            6
+          </Button>
+          <Button value="3" onClick={handleClick}>
+            3
+          </Button>
+          <Button
+            value={
               <img
                 src="https://img.icons8.com/material/24/a7a7a7/clear-symbol--v1.png"
                 alt="clear symbol"
               />
             }
-            handleClick={displayCalc}
-            cName="icons"
-          />
+            id="icons"
+            onClick={handleClick}
+          >
+            <img
+              src="https://img.icons8.com/material/24/a7a7a7/clear-symbol--v1.png"
+              alt="clear symbol"
+            />
+          </Button>
         </div>
         <div className="inner" id="col4">
-          <Clickables val="/" handleClick={displayCalc} />
-          <Clickables val="x" handleClick={displayCalc} />
-          <Clickables val="-" handleClick={displayCalc} />
-          <Clickables val="+" handleClick={displayCalc} />
-          <Clickables val="=" cName="equalsButton" />
+          <Button value="/" onClick={handleClick}>
+            /
+          </Button>
+          <Button value="x" onClick={handleClick}>
+            x
+          </Button>
+          <Button value="-" onClick={handleClick}>
+            -
+          </Button>
+          <Button value="+" onClick={handleClick}>
+            +
+          </Button>
+          <Button val="=" id="equalsButton" onClick={handleClick}>
+            =
+          </Button>
         </div>
       </div>
-      <button onClick={displayResult}>test</button>
+      {/* <button onClick={displayResult}>test</button> */}
     </div>
   );
 }
